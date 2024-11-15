e79b4645900eaf1b3c895f7802c6d76967ceb76f
Valentin Cadin <theoddlyseagull00@gmail.com>
2024-11-15 11:27:39 +0100

    Register bone meal as fertilizer in the ore dictionary

--- src/main/java/com/slava_110/waterlessfarmland/WaterlessFarmlandMod.java
+++ src/main/java/com/slava_110/waterlessfarmland/WaterlessFarmlandMod.java
@@ -1,9 +1,17 @@
 package com.slava_110.waterlessfarmland;
 
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod(modid = "waterlessfarmland")
 public class WaterlessFarmlandMod {
 
-
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        // Register bone meal as "fertilizer" in the ore dictionary
+        OreDictionary.registerOre("fertilizer", new ItemStack(Items.DYE, 1, 15)); // Bone meal has metadata 15
+    }
 }
