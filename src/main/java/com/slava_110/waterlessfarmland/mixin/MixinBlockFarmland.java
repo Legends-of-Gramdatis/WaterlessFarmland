74630804927a560ee6669434fc4f9bb8bbedcb09
slava110 <slava.hero@bk.ru>
2025-02-22 13:20:23 +0300

    Finish farmland fertilization functionality

--- src/main/java/com/slava_110/waterlessfarmland/mixin/MixinBlockFarmland.java
+++ src/main/java/com/slava_110/waterlessfarmland/mixin/MixinBlockFarmland.java
@@ -3,16 +3,9 @@ package com.slava_110.waterlessfarmland.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFarmland;
 import net.minecraft.block.material.Material;
-// import net.minecraft.block.state.IBlockState;
-// import net.minecraft.entity.player.EntityPlayer;
-// import net.minecraft.init.Blocks;
-// import net.minecraft.item.ItemStack;
 import net.minecraft.entity.Entity;
-// import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-// import net.minecraftforge.oredict.OreDictionary;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -42,23 +35,10 @@ abstract class MixinBlockFarmland extends Block {
 
     @Inject(
             method = "hasWater",
-            at = @At("HEAD")
+            at = @At("HEAD"),
+            cancellable = true
     )
     public void hasWater(World worldIn, BlockPos pos, CallbackInfoReturnable<Boolean> ci) {
         ci.setReturnValue(true);
     }
-
-    // @Inject(
-    //         method = "onBlockActivated",
-    //         at = @At("HEAD"),
-    //         cancellable = true
-    // )
-    // public void onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, CallbackInfoReturnable<Boolean> ci) {
-    //     ItemStack heldItem = playerIn.getHeldItem(hand);
-    //     if (!heldItem.isEmpty() && OreDictionary.containsMatch(false, OreDictionary.getOres("fertilizer"), heldItem)) {
-    //         worldIn.setBlockState(pos, Blocks.FARMLAND.getDefaultState().withProperty(BlockFarmland.MOISTURE, 7));
-    //         heldItem.shrink(1);
-    //         ci.setReturnValue(true);
-    //     }
-    // }
 }
